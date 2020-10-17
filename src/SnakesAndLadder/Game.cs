0624e2c632deb73294bf01d71534cdb143acfd42
Pramod Jangam <pjangam@equalexperts.com>
2020-10-17 22:44:39 +0530

    User can proceed in game

--- src/SnakesAndLadder/Game.cs
+++ src/SnakesAndLadder/Game.cs
@@ -1,16 +1,24 @@
+using System;
 using System.Collections.Generic;
 
 namespace SnakesAndLadders
 {
     public class Game
     {
-        private List<Player> _players;
+        public List<Player> Players { get; }
         private Dice _dice;
 
         public Game(List<Player> players, Dice dice)
         {
-            _players = players;
+            Players = players;
             _dice = dice;
+        }
+
+        public int Play()
+        {
+            var diceThrow = _dice.Throw();
+            Players[0].Place += diceThrow;
+            return diceThrow;
         }
     }
 }
--- test/SnakesAndLadders.Tests/SnakeLadderFixture.cs
+++ test/SnakesAndLadders.Tests/SnakeLadderFixture.cs
@@ -19,6 +19,19 @@ namespace SnakesAndLadders.Tests
             //then 
             player.Place.Should().Be(1);
         }
+
+        [Fact]
+        public void GivenNewBoardWithOneUser_WhenGamePlayed_ThenUserIsAdvanced()
+        {
+            //given 
+            var gameBuilder = new GameBuilder();
+            var player = gameBuilder.AddPlayer();
+            var game = gameBuilder.Build();
+            //when 
+            var diceThrow = game.Play();
+            //then 
+            game.Players[0].Place.Should().Be(diceThrow + 1);
+        }
     }
 
 }
