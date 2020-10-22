319b33cf21f53820c3aa7aed1b5e9fda1355d020
Pramod Jangam <pjangam@equalexperts.com>
2020-10-22 21:25:51 +0530

    snake validations

--- src/SnakesAndLadder/Snake.cs
+++ src/SnakesAndLadder/Snake.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace SnakesAndLadders
 {
     public class Snake
@@ -7,6 +9,9 @@ namespace SnakesAndLadders
 
         public Snake(int head, int tail)
         {
+            if (head >= 100) throw new ArgumentOutOfRangeException(nameof(head), head, "Snake head should be less than 100");
+            if (tail <= 1) throw new ArgumentOutOfRangeException(nameof(tail), tail, "Snake tail should be greater than 1");
+            if (head <= tail) throw new ArgumentException("Snake tail has to be below head");
             this.Head = head;
             this.Tail = tail;
         }
